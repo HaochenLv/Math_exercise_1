1bd53945c8013417cd396f6cf4cfd80d13aaea53
Eric Lv <h36345918@gmail.com>
2026-09-06 20:41:13 +0800

    feat: expose Q1 baseline solver

---
+++ src/helicopter_planner/solver/q1/__init__.py
@@ -0,0 +1,13 @@
+from .baseline import (
+    BaselineResult,
+    FacilityDecision,
+    Q1SingleFacilityBaselineSolver,
+    ShuttleRoute,
+)
+
+__all__ = [
+    "BaselineResult",
+    "FacilityDecision",
+    "Q1SingleFacilityBaselineSolver",
+    "ShuttleRoute",
+]
